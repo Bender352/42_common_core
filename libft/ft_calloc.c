77ed2dfa4a9adf5b92c76494d0fa831fd305c45c
Sven <svenbruck@hotmail.com>
2024-10-27 08:17:47 +0100

    updated ft_calloc

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -9,6 +9,6 @@ void *ft_calloc(size_t number_of_elements, size_t el_size)
     buff = malloc(number_of_elements * el_size);
     if (buff == NULL)
         return (NULL);
-    buff = ft_bzero(buff, number_of_elements * el_size);
+    ft_bzero(buff, number_of_elements * el_size);
     return (buff);
 }
